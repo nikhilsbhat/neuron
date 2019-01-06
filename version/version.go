5fa620ee594cd186d812281bb11aa6fb6fc6c570
root <root@ip-172-31-13-218.ap-south-1.compute.internal>
2019-01-06 16:21:39 +0000

    adding version and documentation to create lb

---
+++ version/version.go
@@ -0,0 +1,26 @@
+// This package powers the versioning of the application.
+package version
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// This specifies the version of the application and cannot be changed by end user.
+const versn = "0.0.1"
+
+// This tells end user that what variant (here we use the name of the git branch to make it simple)
+// of application is he using.
+var env = "alfa"
+
+// The function returns the version with variant, and the value will be used to bersion the application.
+// The same version will be displayed at both CLI and app(API).
+func GetVersion() string {
+	var versionString bytes.Buffer
+	fmt.Fprintf(&versionString, "%s", versn)
+	if env != "" {
+		fmt.Fprintf(&versionString, "-%s", env)
+	}
+
+	return versionString.String()
+}
